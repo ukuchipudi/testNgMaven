6f27e40ea4688bf0aed8746f48f38b48d6650bdd
udaybhaskarkuchipudi <udaybhaskarkuchipudi@udays-mbp>
2018-01-17 20:30:51 -0600

    first commit

---
+++ src/test/java/com/javacodegeeks/testng/maven/TestNgMavenExample.java
@@ -0,0 +1,11 @@
+package com.javacodegeeks.testng.maven;
+
+import org.testng.annotations.Test;
+
+public class TestNgMavenExample {
+
+	@Test
+	public void exampleOfTestNgMaven() {
+		System.out.println("This is TestNG-Maven Example");
+	}
+}
---
+++ src/test/java/com/javacodegeeks/testng/maven/TestNgMavenSecondClass.java
@@ -0,0 +1,11 @@
+package com.javacodegeeks.testng.maven;
+
+import org.testng.annotations.Test;
+
+public class TestNgMavenSecondClass {
+
+	@Test
+	public void oneMoreTest() {
+		System.out.println("This is a TestNG-Maven based test");
+	}
+}
